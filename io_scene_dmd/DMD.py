3c5591e0fa1cc15acdc92012e0704e7692da3b55
maisvendoo <maisvendoo@gmail.com>
2018-07-19 00:32:43 +0300

    Вычитаны вершины и грани из DMD-модели

---
+++ io_scene_dmd/DMD.py
@@ -0,0 +1,172 @@
+#-------------------------------------------------------------------------------
+#
+#       Класс для работы с моделями DMD
+#       (c) РГУПС, ВЖД 18/07/2018
+#       Разработал: Притыкин Д.Е.
+#
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+class FileContainer:
+
+    def __init__(self):
+        self.dmd_text = []
+        self.line_index = 0
+        self.length = 0
+
+    #---------------------------------------------------------------------------
+    #
+    #---------------------------------------------------------------------------
+    def load(self, filepath):
+        try:
+            f = open(filepath, 'rt')
+            self.dmd_text = f.read().split('\n')
+            self.line_index = 0
+            self.length = len(self.dmd_text)
+
+            print("Loaded file ", filepath, " with ", self.length, " lines")
+
+            return True
+
+        except Exception as ex:
+            print(ex)
+            return False
+
+    #---------------------------------------------------------------------------
+    #
+    #---------------------------------------------------------------------------
+    def getLine(self):
+        line = ""
+        if self.line_index <= self.length - 1:
+            line = self.dmd_text[self.line_index]
+            self.line_index += 1
+            return line
+        else:
+            return None
+
+    #---------------------------------------------------------------------------
+    #
+    #---------------------------------------------------------------------------
+    def eof(self):
+        return self.line_index > self.length - 1
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+class Mesh:
+
+    def __init__(self):
+        self.vertices = []
+        self.faces = []
+        self.faset_normals = []
+        self.smooth_normals = []
+        self.width = 0.0
+        self.height = 0.0
+        self.depth = 0.0
+        self.vertex_count = 0
+        self.faces_count = 0
+        self.fextent = 0.0
+        self.fextentX = 0.0
+        self.fextentY = 0.0
+        self.fextentZ = 0.0
+        self.scale_type = 0
+
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+class MultyMesh:
+
+    def __init__(self):
+        self.meshes = []
+        self.current_frame = 0
+        self.fextent = 0.0
+        self.fextentX = 0.0
+        self.fextentY = 0.0
+        self.scale_type = 0
+        self.fall_smooth = 0
+        self.fall_scale = 1
+        self.tex_vertices = []
+        self.tex_faces = []
+        self.tex_v_count = 0
+        self.tex_f_count = 0
+        self.texture_present = False
+
+    #---------------------------------------------------------------------------
+    #
+    #---------------------------------------------------------------------------
+    def readNextMesh(self, dmd_cont):
+
+        mesh = Mesh()
+
+        line = dmd_cont.getLine()
+
+        while (line != "numverts numfaces") and line is not None:
+            line = dmd_cont.getLine()
+
+        line = dmd_cont.getLine()
+        geom_data = line.split(" ")
+
+        tmp = []
+        for gd in geom_data:
+            try:
+                tmp.append(int(gd))
+            except ValueError:
+                pass
+
+        mesh.vertex_count = tmp[0]
+        mesh.faces_count = tmp[1]
+
+        line = dmd_cont.getLine()
+
+        for i in range(0, mesh.vertex_count):
+            line = dmd_cont.getLine()
+            vertex_data = line.strip('\t').split(" ")
+            try:
+                x = float(vertex_data[0])
+                y = float(vertex_data[1])
+                z = float(vertex_data[2])
+                mesh.vertices.append([x, y, z])
+            except Exception as ex:
+                print(ex)
+                return
+
+        line = dmd_cont.getLine()
+        line = dmd_cont.getLine()
+
+        for i in range(0, mesh.faces_count):
+            line = dmd_cont.getLine()
+            face_data = line.rstrip('\t').split(" ")
+            face = []
+            for f in face_data:
+                try:
+                    face.append(int(f))
+                except Exception as ex:
+                    print(ex)
+                    return
+
+            mesh.faces.append(face)
+
+        self.meshes.append(mesh)
+
+    #---------------------------------------------------------------------------
+    #
+    #---------------------------------------------------------------------------
+    def loadFromFile(self, filepath):
+        dmd_cont = FileContainer()
+        dmd_cont.load(filepath)
+
+        line = dmd_cont.getLine()
+        while  line is not None:
+            if line == "New object":
+                self.readNextMesh(dmd_cont)
+
+            if line == "New Texture:":
+                pass
+
+            line = dmd_cont.getLine()
+
+
+
